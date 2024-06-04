a10c63597c3850c53d7b5d6253759ba0a50775ef
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-06-04 17:50:32 -0300

    exemplos_tipos e variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        //Tipos primitivos
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 11122281; 
+        long cpf = 98765432109L;
+        float salarioMinino = 2500f;
+        double salarioMaximo = 2500.33;
+
+        String nome = "Henrique";
+        Array nomes [];
+        //final usado para consoante
+        final int NUMERO = 2;
+        
+        System.out.println(NUMERO);
+    }
+}
